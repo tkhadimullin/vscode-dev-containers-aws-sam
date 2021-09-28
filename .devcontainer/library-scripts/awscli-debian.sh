e569a11aaeab5d0df907472ba3c7ba1aa932ea5a
T Khadimulin <khadimulin@gmail.com>
2021-09-28 22:28:01 +1300

    Initial commit

---
+++ .devcontainer/library-scripts/awscli-debian.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm -rf ./aws
+rm ./awscliv2.zip
+echo "AWS CLI version `aws --version`"
+
+curl -L "https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip" -o "aws-sam-cli-linux-x86_64.zip"
+unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
+sudo ./sam-installation/install
+echo "SAM version `sam --version`"
+rm -rf ./sam-installation
+rm ./aws-sam-cli-linux-x86_64.zip
+
+wget https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-3.1
+
+dotnet tool install -g Amazon.Lambda.Tools
+export PATH="$PATH:$HOME/.dotnet/tools"
